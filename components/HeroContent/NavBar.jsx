731e49d660dae02b34d996a0c9381feafef5a13b
Abraham Pech <abrahamayil415@gmail.com>
2026-01-27 19:19:46 -0600

    Setting styles and responsive design

--- components/HeroContent/NavBar.jsx
+++ components/HeroContent/NavBar.jsx
@@ -1,22 +1,49 @@
 import "./NavBar.css";
 
 const NavBar = () => {
-    return(
-        <header>
-            <nav>
-                <div className="name">
-                    <img src="" alt="Person" />
-                    <h2>ABRAHAM PECH</h2>
-                </div>
-                <ul className="links">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#about-me">About me</a></li>
-                    <li><a href="#skills">Skills</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header>
+      <nav>
+        <div className="name">
+          <img src="../../../public/img/avatar.png" alt="Avatar" />
+          <h2>ABRAHAM PECH</h2>
+        </div>
+        <ul className="links">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#about-me">About me</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+        </ul>
+
+        <div className="toggle">
+          <img src="../../../public/img/bars.png" alt="Bars" />
+        </div>
+
+        <ul className="menu-responsive">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#about-me">About me</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default NavBar;
