473624bd432008559e5fb33699d8d7a53a042f25
Abraham Pech <abrahamayil415@gmail.com>
2026-01-28 19:14:26 -0600

    Making about me section and its responsive design

--- components/AboutMe/AboutMe.jsx
+++ components/AboutMe/AboutMe.jsx
@@ -1,3 +1,5 @@
+import "./AboutMe.css";
+
 const AboutMe = () => {
   return (
     <section className="about-me" id="about-me">
@@ -5,25 +7,27 @@ const AboutMe = () => {
         <div className="content">
           <div className="titles">
             <h2>ABOUT ME</h2>
-            <h3>Who Am I?</h3>
+            <h3>Who am I?</h3>
           </div>
-          <p className="text">
-            I am a Programming Engineering and Webmaster student, currently
-            focused on becoming a Full Stack Developer. Although I am still
-            pursuing my academic studies, I have worked on several academic and
-            personal projects that have allowed me to gain hands-on experience
-            in building functional and efficient applications.
-          </p>
-          <p className="another-text">
-            My professional goal is to join a development team where I can
-            contribute my skills, continue learning, and grow as a software
-            developer. I am passionate about solving problems through code,
-            applying best practices, and creating solutions that deliver real
-            value. This website showcases a selection of my projects to
-            demonstrate my abilities, commitment, and approach to development.
-          </p>
+          <div className="div-text">
+            <p className="text first-text">
+              I am a Programming Engineering and Webmaster student, currently
+              focused on becoming a Full Stack Developer. Although I am still
+              pursuing my academic studies, I have worked on several academic
+              and personal projects that have allowed me to gain hands-on
+              experience in building functional and efficient applications.
+            </p>
+            <p className="text second-text">
+              My professional goal is to join a development team where I can
+              contribute my skills, continue learning, and grow as a software
+              developer. I am passionate about solving problems through code,
+              applying best practices, and creating solutions that deliver real
+              value. This website showcases a selection of my projects to
+              demonstrate my abilities, commitment, and approach to development.
+            </p>
+          </div>
         </div>
-        <img src="#" alt="Abraham Pech" />
+        <img src="../../../public/img/ap.jpg" alt="Abraham Pech" />
       </div>
     </section>
   );
