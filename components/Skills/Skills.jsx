7e140080f6338a6eb72e2fc68aca8149f506d0f2
Abraham Pech <abrahamayil415@gmail.com>
2026-01-29 20:00:51 -0600

    Setting images for skills section

--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -1,31 +1,33 @@
+import "./Skills.css";
+
 const Skills = () => {
   return (
     <section className="skills" id="skills">
-      <div className="languages">
-        <h3>Language skills</h3>
+      <div className="programming">
+        <h3>Tech skills</h3>
         <div className="content">
           <div className="skill">
-            <img src="#" alt="HTML" />
+            <img src="../../../public/img/skills/html.webp" alt="HTML" />
             <p>HTML</p>
           </div>
           <div className="skill">
-            <img src="#" alt="CSS" />
+            <img src="../../../public/img/skills/css.webp" alt="CSS" />
             <p>CSS</p>
           </div>
           <div className="skill">
-            <img src="#" alt="JS" />
+            <img src="../../../public/img/skills/js.png" alt="JS" />
             <p>JavaScript</p>
           </div>
           <div className="skill">
-            <img src="#" alt="TS" />
+            <img src="../../../public/img/skills/ts.png" alt="TS" />
             <p>TypeScript</p>
           </div>
           <div className="skill">
-            <img src="#" alt="PHP" />
+            <img src="../../../public/img/skills/php.png" alt="PHP" />
             <p>PHP</p>
           </div>
           <div className="skill">
-            <img src="#" alt="MySQL" />
+            <img src="../../../public/img/skills/mysql.webp" alt="MySQL" />
             <p>MySQL</p>
           </div>
         </div>
@@ -33,56 +35,89 @@ const Skills = () => {
       <div className="frameworks">
         <h3>Frameworks/libraries</h3>
         <div className="content">
-            <div className="skill">
-                <img src="" alt="Bootstrap" />
-                <p>Bootstrap</p>
-            </div>
-            <div className="skill">
-                <img src="" alt="Tailwind" />
-                <p>Tailwind</p>
-            </div>
-            <div className="skill">
-                <img src="" alt="Laravel" />
-                <p>Laravel</p>
-            </div>
-            <div className="skill">
-                <img src="" alt="React" />
-                <p>React</p>
-            </div>
-            <div className="skill">
-                <img src="" alt="Node.js" />
-                <p>Node.js</p>
-            </div>
-            <div className="skill">
-                <img src="" alt="Vite.js" />
-                <p>Vite.js</p>
-            </div>
+          <div className="skill">
+            <img
+              src="../../../public/img/skills/bootstrap.png"
+              alt="Bootstrap"
+            />
+            <p>Bootstrap</p>
+          </div>
+          <div className="skill">
+            <img src="../../../public/img/skills/tailwind.svg" alt="Tailwind" />
+            <p>Tailwind</p>
+          </div>
+          <div className="skill">
+            <img src="../../../public/img/skills/laravel.jfif" alt="Laravel" />
+            <p>Laravel</p>
+          </div>
+          <div className="skill">
+            <img src="../../../public/img/skills/react.webp" alt="React" />
+            <p>React</p>
+          </div>
+          <div className="skill">
+            <img src="../../../public/img/skills/nodejs.png" alt="Node.js" />
+            <p>Node.js</p>
+          </div>
+          <div className="skill">
+            <img src="../../../public/img/skills/vitejs.png" alt="Vite.js" />
+            <p>Vite.js</p>
+          </div>
+        </div>
+      </div>
+      <div className="languages">
+        <h3>Language skills</h3>
+        <div className="content">
+          <div className="skill">
+            <img src="../../../public/img/skills/mexico.webp" alt="React" />
+            <p>Spanish C2 (Native Language)</p>
+          </div>
+          <div className="skill">
+            <img src="../../../public/img/skills/english.svg" alt="Node.js" />
+            <p>English C2</p>
+          </div>
+          <div className="skill">
+            <img src="../../../public/img/skills/italian.webp" alt="Vite.js" />
+            <p>Italian A2</p>
+          </div>
         </div>
       </div>
       <div className="social">
+        <h3>Soft skills</h3>
         <div className="skill">
-            <img src="" alt="Teamwork" />
-            <p>Teamwork</p>
+          <img src="../../../public/img/skills/teamwork.png" alt="Teamwork" />
+          <p>Teamwork</p>
         </div>
         <div className="skill">
-            <img src="" alt="Leadership" />
-            <p>Leadership</p>
+          <img
+            src="../../../public/img/skills/leadership.jpg"
+            alt="Leadership"
+          />
+          <p>Leadership</p>
         </div>
         <div className="skill">
-            <img src="" alt="Communication" />
-            <p>Communication</p>
+          <img
+            src="../../../public/img/skills/communication.png"
+            alt="Communication"
+          />
+          <p>Communication</p>
         </div>
         <div className="skill">
-            <img src="" alt="Emotional Intelligence" />
-            <p>Emotional Intelligence</p>
+          <img
+            src="../../../public/img/skills/ei.png"
+            alt="Emotional Intelligence"
+          />
+          <p>Emotional Intelligence</p>
         </div>
         <div className="skill">
-            <img src="" alt="Organization" />
-            <p>Organization</p>
+          <img
+            src="../../../public/img/skills/organization.png"
+            alt="Organization"
+          />
+          <p>Organization</p>
         </div>
         <div className="skill">
-            <img src="" alt="Troubleshooting" />
-            <p>Troubleshooting</p>
+          <img src="../../../public/img/skills/tbs.png" alt="Troubleshooting" />
+          <p>Troubleshooting</p>
         </div>
       </div>
     </section>
