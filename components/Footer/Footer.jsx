3f044fb5693bd1aad92e88cd0c46f725d40242b9
Abraham Pech <abrahamayil415@gmail.com>
2026-02-03 19:36:47 -0600

    Making the footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,22 +1,37 @@
+import "./Footer.css";
+
 const Footer = () => {
   return (
     <footer>
-      <div className="text">
-        <p>
-          This portfolio was built with React and other libraries. Source code
-          available on Github: <br></br> https://github.com/abrahampa21/portfolio 
-        </p>
-        <img src="#" alt="React" />
-      </div>
-      <div className="contact">
-        <p>Contact</p>
-        <div className="icons">
-            <a><img src="#" alt="LinkedIn" /></a>
-            <a><img src="#" alt="Github" /></a>
-            <a><img src="#" alt="Gmail" /></a>
+      <div className="footer-content">
+        <div className="text">
+          <p>
+            This portfolio was built with React and other libraries. Source code
+            available on Github: <br></br>{" "}
+            https://github.com/abrahampa21/portfolio
+          </p>
+          <img src="../../../public/img/skills/react.webp" alt="React" />
         </div>
+        <div className="contact">
+          <p>Contact</p>
+          <div className="icons">
+            <a>
+              <img src="../../../public/img/linkedin.png" alt="LinkedIn" />
+            </a>
+            <a>
+              <img src="../../../public/img/github.png" alt="Github" />
+            </a>
+            <a>
+              <img src="../../../public/img/gmail.webp" alt="Gmail" />
+            </a>
+          </div>
+        </div>
+        <img
+          src="../../../public/img/logo.png"
+          className="logo-ap"
+          alt="Abraham Pech"
+        />
       </div>
-      <img src="#" alt="Abraham Pech" />
       <p className="copyright">All rights reserved AAPA &copy; 2026</p>
     </footer>
   );
