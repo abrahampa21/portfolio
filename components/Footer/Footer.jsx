751acf87b8db7b1eeea74d95a3a71d74fb7d793a
Abraham Pech <abrahamayil415@gmail.com>
2026-02-05 19:59:16 -0600

    Making a responsive footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -8,31 +8,35 @@ const Footer = () => {
           <p>
             This portfolio was built with React and other libraries. Source code
             available on Github: <br></br>{" "}
-            https://github.com/abrahampa21/portfolio
+            <a href="https://github.com/abrahampa21/portfolio" target="_blank">
+              https://github.com/abrahampa21/portfolio
+            </a>
           </p>
           <img src="../../../public/img/skills/react.webp" alt="React" />
         </div>
         <div className="contact">
-          <p>Contact</p>
           <div className="icons">
-            <a>
-              <img src="../../../public/img/linkedin.png" alt="LinkedIn" />
-            </a>
-            <a>
-              <img src="../../../public/img/github.png" alt="Github" />
-            </a>
-            <a>
-              <img src="../../../public/img/gmail.webp" alt="Gmail" />
-            </a>
+            <p>Contact</p>
+            <div className="icons-media">
+              <a>
+                <img src="../../../public/img/linkedin.png" alt="LinkedIn" />
+              </a>
+              <a>
+                <img src="../../../public/img/github.png" alt="Github" />
+              </a>
+              <a>
+                <img src="../../../public/img/gmail.webp" alt="Gmail" />
+              </a>
+            </div>
           </div>
+          <img
+            src="../../../public/img/logo.png"
+            className="logo-ap"
+            alt="Abraham Pech"
+          />
         </div>
-        <img
-          src="../../../public/img/logo.png"
-          className="logo-ap"
-          alt="Abraham Pech"
-        />
       </div>
-      <p className="copyright">All rights reserved AAPA &copy; 2026</p>
+      <p className="copyright">All rights reserved Abraham Pech &copy; 2026</p>
     </footer>
   );
 };
