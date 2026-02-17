7be58860e4de229af0040722fe63a7852bb2fe0c
Abraham Pech <abrahamayil415@gmail.com>
2026-02-16 19:20:05 -0600

    Improving projects cards and making the modal

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -1,12 +1,17 @@
 import "./Projects.css";
 import "@splidejs/react-splide/css";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { useState } from "react";
+import projectsData from "./Projects.js";
 
 const Projects = () => {
+  const [selectedProject, setSelectedProject] = useState(null);
+
   return (
     <section className="projects" id="projects">
       <h3>Projects</h3>
       <div className="projects">
+        {/*Carousel*/}
         <Splide
           className="splide-container"
           options={{
@@ -29,61 +34,47 @@ const Projects = () => {
           }}
           aria-label="My Favorite Images"
         >
-          <SplideSlide className="card-slide">
-            <img
-              src="/img/projects/decori.webp"
-              loading="lazy"
-              alt="Ticket sale managment project"
-            />
-            <div className="content">
-              <h4>Ticket sales</h4>
-              <button href="#" className="see-more">See more</button>
-            </div>
-          </SplideSlide>
-          <SplideSlide className="card-slide">
-            <img src="/img/projects/dc.webp" alt="Dulces Cuidados" />
-            <div className="content">
-              <h4>Dulces Cuidados</h4>
-              <button href="" className="see-more">See more</button>
-            </div>
-          </SplideSlide>
-          <SplideSlide className="card-slide">
-            <img src="/img/projects/iteslogo.webp" alt="Vocational Test" />
-            <div className="content">
-              <h4>Vocational Test</h4>
-              <button href="#" className="see-more">See more</button>
-            </div>
-          </SplideSlide>
-          <SplideSlide className="card-slide">
-            <img src="/img/projects/academix.webp" alt="Academix" />
-            <div className="content">
-              <h4>Academix</h4>
-              <button href="#" className="see-more">See more</button>
-            </div>
-          </SplideSlide>
-          <SplideSlide className="card-slide">
-            <img src="/img/projects/codelab.webp" alt="Codelab" />
-            <div className="content">
-              <h4>CodeLab</h4>
-              <button href="#" className="see-more">See more</button>
-            </div>
-          </SplideSlide>
-          <SplideSlide className="card-slide">
-            <img src="/img/projects/tiktime.webp" alt="tiktime" />
-            <div className="content">
-              <h4>Tik-Time</h4>
-              <button href="#" className="see-more">See more</button>
-            </div>
-          </SplideSlide>
-          <SplideSlide className="card-slide">
-            <img src="/img/projects/encryp.png" alt="Java Encryptor" />
-            <div className="content">
-              <h4>Java Encryptor</h4>
-              <button href="#" className="see-more">See more</button>
-            </div>
-          </SplideSlide>
+          {projectsData.map((proj, index) => (
+            <SplideSlide key={index} className="card-slide">
+              <img src={proj.image} loading="lazy" alt={proj.title} />
+              <div className="content">
+                <h4>{proj.title}</h4>
+                <button
+                  className="see-more"
+                  onClick={() => setSelectedProject(proj)}
+                >
+                  See more
+                </button>
+              </div>
+            </SplideSlide>
+          ))}
         </Splide>
       </div>
+
+      {/*Modal*/}
+      {selectedProject &&(
+        <div className="modal" onClick={() => setSelectedProject(null)}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <span className="close" onClick={() => setSelectedProject(null)}>&times;</span>
+            <h2>{selectedProject.title}</h2>
+            <img src={selectedProject.image}  alt={selectedProject.title} className="modal-image" />
+            <p>{selectedProject.description}</p>
+
+            <div className="tech-container">
+              {selectedProject.tech.map((tech, i) => (
+                <span key={i} className="tech-badge">
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            <div className="modal-buttons">
+              <a href={setSelectedProject.demo || "#"} className="demo-btn">Live Demo</a>
+              <a href={setSelectedProject.github || "#"} className="github-btn">Github</a>
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
