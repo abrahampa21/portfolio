daf6abea1a2995c66e847d4e11cbd4d4ff4eb735
Abraham Pech <abrahamayil415@gmail.com>
2026-04-01 09:10:06 -0600

    Improving styles

--- App.jsx
+++ App.jsx
@@ -4,8 +4,23 @@ import Footer from "./components/Footer/Footer";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import "./index.css";
+import { useState, useEffect } from "react";
+import Loader from "./components/Loader";
 
 const App = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 1200);
+
+        return () => clearTimeout(timer);
+    },[]);
+
+    if(loading){
+        return <Loader/>
+    }
     return(
         <div className="content">
             <HeroContent/>
---
+++ components/Loader.jsx
@@ -0,0 +1,9 @@
+import "../index.css";
+
+export default function Loader() {
+  return (
+    <div id="loader">
+      <div className="spinner"></div>
+    </div>
+  );
+}
