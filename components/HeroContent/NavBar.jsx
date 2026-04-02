2c793c3ce20107e4d63a327162ec34bcf54eaefe
Abraham Pech <abrahamayil415@gmail.com>
2026-04-01 19:20:23 -0600

    Improve responsive design and small changes in every section

--- components/HeroContent/NavBar.jsx
+++ components/HeroContent/NavBar.jsx
@@ -1,6 +1,17 @@
 import "./NavBar.css";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header>
       <nav>
@@ -23,22 +34,24 @@ const NavBar = () => {
           </li>
         </ul>
 
-        <div className="toggle">
-          <img src="img/bars.png" alt="Bars" />
+        <div className={`toggle ${isMenuOpen ? "active" : ""}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
 
-        <ul className="menu-responsive">
+        <ul className={`menu-responsive ${isMenuOpen ? "active" : ""}`}>
           <li>
-            <a href="#">Home</a>
+            <a href="#" onClick={closeMenu}>Home</a>
           </li>
           <li>
-            <a href="#about-me">About me</a>
+            <a href="#about-me" onClick={closeMenu}>About me</a>
           </li>
           <li>
-            <a href="#skills">Skills</a>
+            <a href="#skills" onClick={closeMenu}>Skills</a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="#projects" onClick={closeMenu}>Projects</a>
           </li>
         </ul>
       </nav>
